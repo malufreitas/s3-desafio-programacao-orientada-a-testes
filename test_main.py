b197bbb269f732a9651e332b5fec1fbe26a6197f
malufreitas <malufrt@Hotmail.com>
2019-09-11 22:47:07 -0300

    Desafio concluído com sucesso
    Utilizando monkeypatching para mockar os dados de retorno da api

--- test_main.py
+++ test_main.py
@@ -1,14 +1,39 @@
 from main import get_temperature
 
-def test_get_temperature_by_lat_lng():
-    # Setup
-    lat = -14.235004
-    lng = -51.92528
-    temperature = 62
-    expected = 16
+import requests
+import pytest
+
+'''
+lat = -14.235004
+lng = -51.92528
+temperature = 62
+expected = 16
+'''
+
+# Classe para retornar o valor mockado
+# Ele sobrescreve o requests.Response que é retornado pelo requests.get
+class MockResponse:
+
+    @staticmethod
+    def json():
+        return {"currently": {"temperature": 62}}
+
+
+#@pytest.mark.parametrize('lat, lng, expected', [(-14.235004,-51.92528,16)])  # Setup 
+@pytest.mark.parametrize('lat', [-14.235004])  # Setup 
+@pytest.mark.parametrize('lng', [-51.92528])  # Setup 
+@pytest.mark.parametrize('expected', [16])  # Setup 
+def test_get_temperature_by_lat_lng(monkeypatch, lat, lng, expected):
+
+    # Mock_get sempre vai retornar o objeto do metodo json()
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    # Usa o monkeypatch para a requisição requests.get no mock_get
+    monkeypatch.setattr(requests, "get", mock_get)
 
     # Execução do teste
-    resp = get_temperature(lat, lng)
+    result = get_temperature(lat, lng)
 
     # Verificação do resultado do teste
-    assert resp is expected
+    assert result is expected
